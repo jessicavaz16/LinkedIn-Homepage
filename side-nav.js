7235720b2575fa7b9d5dafd798a683aa7b4e451d
Jessica Vaz <56466050+jessicavaz16@users.noreply.github.com>
2020-07-14 23:06:50 +0530

    Update side-nav.js

--- side-nav.js
+++ side-nav.js
@@ -10,9 +10,6 @@ class SideNav {
     this.showSideNav = this.showSideNav.bind(this);
     this.hideSideNav = this.hideSideNav.bind(this);
     this.blockClicks = this.blockClicks.bind(this);
-    this.onTouchStart = this.onTouchStart.bind(this);
-    this.onTouchMove = this.onTouchMove.bind(this);
-    this.onTouchEnd = this.onTouchEnd.bind(this);
     this.onTransitionEnd = this.onTransitionEnd.bind(this);
     this.startX = 0;
     this.currentX = 0;
@@ -24,33 +21,9 @@ class SideNav {
     this.hideButtonEl.addEventListener("click", this.hideSideNav);
     this.sideNavEl.addEventListener("click", this.hideSideNav);
     this.sideNavContainerEl.addEventListener("click", this.blockClicks);
-
-    document.addEventListener("touchstart", this.onTouchStart);
-    document.addEventListener("touchmove", this.onTouchMove);
-    document.addEventListener("touchend", this.onTouchEnd);
   }
 
-  onTouchStart(evt) {
-    if (!this.sideNavEl.classList.contains("side-nav--visible")) return;
-
-    this.startX = evt.touches[0].pageX;
-    this.currentX = this.startX;
-  }
-  onTouchMove(evt) {
-    this.currentX = evt.touches[0].pageX;
-    const translatex = Math.min(0, this.currentX - this.startX);
-    if (translatex < 0) {
-      evt.preventDefault();
-    }
-    this.sideNavContainerEl.style.transform = `translateX(${translatex}px)`;
-  }
-
-  onTouchEnd(evt) {
-    const translatex = Math.min(0, this.currentX - this.startX);
-    if (translatex < 0) {
-      this.sideNavContainerEl.style.transform = "";
-      this.hideSideNav();
-    }
+  
   }
   blockClicks(evt) {
     evt.stopPropagation();
